8a91efa656e8aee57c287c2d025558d82fc2dd0b
rin-bamboo <78258130+Rin-bamboo@users.noreply.github.com>
2025-05-27 23:13:17 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorChatApp/BlazorChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ILogger<ChatHub> _logger;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            _logger = logger;
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            _logger.LogInformation("メッセージ受信: {User}: {Message}", user, message);
+            // Send the message to all connected clients
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }   
+    }
+}
